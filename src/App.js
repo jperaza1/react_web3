1673c4b2e65924498cc2dd43b3d6c60cbc2d8c53
jperazas208 <jperaza@mayoreoexp.com>
2022-01-24 11:38:50 -0600

    agregando toast

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { useEffect } from 'react'
+import { ToastContainer } from 'react-toastify';
 import Navbar from './components/Navbar/Navbar';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <Navbar />
+      <ToastContainer 
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+      />
     </>
   );
 }
